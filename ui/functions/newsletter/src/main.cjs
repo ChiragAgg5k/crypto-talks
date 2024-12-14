9f0ac1594d713a127d0355c47e43f791565cbeb3
ChiragAgg5k <chiragaggarwal5k@gmail.com>
2024-12-14 12:16:12 +0530

    fix: functions

--- ui/functions/newsletter/src/main.cjs
+++ ui/functions/newsletter/src/main.cjs
@@ -1,17 +1,55 @@
 const sdk = require('node-appwrite');
-const formData = require('form-data');
-const Mailgun = require('mailgun.js');
+const { Resend } = require('resend');
+const fs = require('fs');
+const path = require('path');
+const querystring = require('node:querystring');
+const staticFolder = path.join(__dirname, '../static');
 
-module.exports = async function (req, res) {
-  if (!req || !res) {
-    throw new Error('Request and response objects are required');
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+const ErrorCode = {
+  INVALID_REQUEST: 'invalid-request',
+  MISSING_FORM_FIELDS: 'missing-form-fields',
+  SERVER_ERROR: 'server-error',
+};
+
+function urlWithCodeParam(baseUrl, codeParam) {
+  const url = new URL(baseUrl);
+  url.searchParams.set('code', codeParam);
+  return url.toString();
+}
+
+function getStaticFile(fileName) {
+  return fs.readFileSync(path.join(staticFolder, fileName)).toString();
+}
+
+module.exports = async function ({ req, res, log, error }) {
+  if (!process.env.ALLOWED_ORIGINS || process.env.ALLOWED_ORIGINS === '*') {
+    log(
+      'WARNING: Allowing requests from any origin - this is a security risk!'
+    );
   }
 
-  if (req.method === 'GET') {
-    return res.json({
-      status: 'OK',
-      message: 'Newsletter function is up and running',
+  if (req.method === 'GET' && req.path === '/') {
+    return res.text(getStaticFile('index.html'), 200, {
+      'Content-Type': 'text/html; charset=utf-8',
     });
+  }
+
+  if (req.headers['content-type'] !== 'application/x-www-form-urlencoded') {
+    error('Incorrect content type.');
+    return res.redirect(
+      urlWithCodeParam(req.headers['referer'], ErrorCode.INVALID_REQUEST)
+    );
+  }
+
+  const form = querystring.parse(req.body);
+
+  if (form['password'] !== process.env.PASSWORD) {
+    error('Invalid password.');
+    return res.redirect(
+      urlWithCodeParam(req.headers['referer'], ErrorCode.INVALID_REQUEST)
+    );
   }
 
   const client = new sdk.Client();
@@ -22,19 +60,12 @@ module.exports = async function (req, res) {
     .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
     .setKey(process.env.APPWRITE_FUNCTION_API_KEY);
 
-  const mailgun = new Mailgun(formData);
-  const mg = mailgun.client({
-    username: 'api',
-    key: process.env.MAILGUN_API_KEY,
-  });
-
   try {
     const subscribers = await database.listDocuments(
       'crypto_portfolio',
       'subscribers',
       [sdk.Query.equal('subscribed', true)]
     );
-
     const newsletterContent = {
       subject: 'Your Monthly Newsletter',
       html: '<h1>Welcome to our Newsletter!</h1><p>Here are the latest updates...</p>',
@@ -42,25 +73,24 @@ module.exports = async function (req, res) {
 
     const emailPromises = subscribers.documents.map((subscriber) => {
       const data = {
-        from: req.variables['SENDER_EMAIL'],
-        to: subscriber.email,
+        from: 'Crypto Talks <crypto@chiragaggarwal.tech>',
+        to: [subscriber.email],
         subject: newsletterContent.subject,
         html: newsletterContent.html,
       };
-      return mg.messages.create('smtp.mailgun.org', data);
+
+      return resend.emails.send(data);
     });
 
     await Promise.all(emailPromises);
 
-    res.json({
-      success: true,
-      message: `Newsletter sent to ${subscribers.documents.length} subscribers.`,
+    return res.text(getStaticFile('success.html'), 200, {
+      'Content-Type': 'text/html; charset=utf-8',
     });
   } catch (error) {
-    res.json({
-      success: false,
-      message: 'Failed to send newsletter',
-      error: error.message,
-    });
+    console.log(error);
+    return res.redirect(
+      urlWithCodeParam(req.headers['referer'], ErrorCode.SERVER_ERROR)
+    );
   }
 };
