c2e978f2ea2410d3cc0271dc259a2684c0b8a565
ChiragAgg5k <chiragaggarwal5k@gmail.com>
2024-12-15 19:03:58 +0530

    docs: added sentry

--- ui/next.config.ts
+++ ui/next.config.ts
@@ -1,10 +1,29 @@
+import { withSentryConfig } from "@sentry/nextjs";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   eslint: {
     ignoreDuringBuilds: true,
   },
 };
 
-export default nextConfig;
+export default withSentryConfig(nextConfig, {
+  org: "chirag-aggarwal",
+  project: "crypto-talks",
+  silent: !process.env.CI,
+
+  widenClientFileUpload: true,
+
+  reactComponentAnnotation: {
+    enabled: true,
+  },
+
+  tunnelRoute: "/monitoring",
+  hideSourceMaps: true,
+  disableLogger: true,
+  automaticVercelMonitors: true,
+
+  sourcemaps: {
+    disable: true,
+  },
+});
